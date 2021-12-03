3bda6493b2045aa15f847c680ad3307be275d9b4
Jemisson <jemison321@gmail.com>
2021-12-02 21:59:09 -0400

    Finalizando modulo 02

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react"
 import ListaDeNotas from "./components/ListaDeNotas"
 import FormularioCadastro from "./components/FormularioCadastro"
+import ListaDeCategorias from './components/ListaDeCategorias'
 import "./assets/App.css"
 import './assets/index.css'
 
@@ -8,32 +9,47 @@ class App extends Component {
 
   constructor(){
     super();
+
     this.state = {
-      notas: []
+      notas: [],
+      categorias: [],
     }
   }
 
-  apagarNota(){
-    console.log('jkdsfjkds');
+  apagarNota(index){
+    let arrayNotas = this.state.notas;
+    arrayNotas.splice(index,1);
+    this.setState({notas: arrayNotas})
   }
 
   criarNota(titulo, texto){
     const novaNota = {titulo, texto};
-    const novoArrayNotas = [...this.state.notas, novaNota]
+    const novoArrayNotas = [...this.state.notas, novaNota];
     const novoEstado = {
-      notas: novoArrayNotas
+      notas: novoArrayNotas,
     }
-    this.setState(novoEstado)
+    this.setState(novoEstado);
 
+  }
+
+  adicionarCategoria(nomeCategoria){
+    const novoArrayCategorias = [...this.state.categorias, nomeCategoria]
+    const novoEstado = {...this.state, categorias: novoArrayCategorias};
+    this.setState(novoEstado);
   }
 
   render() {
     return (
       <section className="conteudo">
         <FormularioCadastro criarNota={this.criarNota.bind(this)}/>
+        <main className="conteudo-principal">
+          <ListaDeCategorias
+            adicionarCategoria={this.adicionarCategoria.bind(this)}
+            categorias={this.state.categorias}/>
         <ListaDeNotas
           apagarNota={this.apagarNota.bind(this)}
           notas={this.state.notas}/>
+      </main>
       </section>
     );
   }
